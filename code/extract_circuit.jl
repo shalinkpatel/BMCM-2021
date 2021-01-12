d06f148daa9e7b569c945782f9ee7d907d594344
Shalin Patel <shalin.kp.patel@gmail.com>
2021-01-12 12:05:45 -0500

    removed large file temp

---
+++ code/extract_circuit.jl
@@ -0,0 +1,11 @@
+using DataFrames, DataFramesMeta, CSV
+
+circuit = 14
+
+dataset = CSV.File("../data/FinalDataset.csv") |> DataFrame
+subset = @linq dataset |> 
+    where(:CIRCDIST .== circuit) |> 
+    select(:AGE, :CIRCDIST, :CITIZEN, :CRIMHIST, :CRIMPTS, :judge, :NEWEDUC,
+            :NEWRACE, :MONSEX, :OFFICE, :PRISTOT, :PROBATN, :SENTDATE, :SENTMON, :SENTYR)
+
+CSV.write("../data/circuit$(circuit).csv", subset)
